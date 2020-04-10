4feed46df9fe0f366b31cb8357a29868bc29fcd4
krautbax <20214119+krautbax@users.noreply.github.com>
2020-04-10 14:07:16 -0400

    first testcase

---
+++ pkg/hello/hello_test.go
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020 krautbax.
+ * Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package hello
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestGreeting(t *testing.T) {
+	if g := Greeting(); !strings.HasPrefix(g, "Hello, World!") {
+		t.Errorf("Greeting() is %v", g)
+	}
+}
